dccbaf77b7d84f215ef7348a2137b7df43883006
Derek Frankhouser <derek.frankhouser@gmail.com>
2026-08-20 22:17:15 -0400

    Clean up temporary inspection scratch scripts

--- check_anom.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('data/hifld_primary_raw.json', 'r') as f:
-    data = json.load(f)
-
-for feat in data:
-    attrs = feat.get('attributes', {})
-    if attrs.get('NAME') == 'FULTON COUNTY JAIL':
-        print('Raw FULTON COUNTY JAIL:', attrs.get('CAPACITY'), attrs.get('POPULATION'))
